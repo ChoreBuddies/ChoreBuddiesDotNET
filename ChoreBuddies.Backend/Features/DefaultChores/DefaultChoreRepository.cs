84d35b041c43972c0702e9f2326696c6c5707c38
kubadrazan <kubadrazan2@gmail.com>
2025-08-31 15:54:08 +0200

    Start implementing default chore repository

---
+++ ChoreBuddies.Backend/Features/DefaultChores/DefaultChoreRepository.cs
@@ -0,0 +1,20 @@
+﻿using ChoreBuddies.Backend.Domain;
+using ChoreBuddies.Database;
+
+namespace ChoreBuddies.Backend.Features.DefaultChores
+{
+    public interface IDefaultChoreRepository
+    {
+        public Task<ICollection<DeafultChore>> GetAllDefaultChores();
+    }
+
+    public class DefaultChoreRepository(ChoreBuddiesDbContext dbContext) : IDefaultChoreRepository
+    {
+        private ChoreBuddiesDbContext _dbContext = dbContext;
+
+        public Task<ICollection<DeafultChore>> GetAllDefaultChores()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
