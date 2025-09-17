e05175c6cb636e75612b28a88f2a2f11f2bbb7c4
kubadrazan <kubadrazan2@gmail.com>
2025-09-17 22:12:57 +0200

    modify default chore

---
+++ ChoreBuddies.Backend/Infrastructure/Data/Migrations/20250917201246_ChangeDefaultChoresColumns.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+
+#nullable disable
+
+namespace ChoreBuddies.Backend.Infrastructure.Data.Migrations;
+
+/// <inheritdoc />
+public partial class ChangeDefaultChoresColumns : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "DueDate",
+            table: "DefaultChores");
+
+        migrationBuilder.AddColumn<int>(
+            name: "ChoreDuration",
+            table: "DefaultChores",
+            type: "int",
+            nullable: false,
+            defaultValue: 0);
+
+        migrationBuilder.AddColumn<string>(
+            name: "Frequency",
+            table: "DefaultChores",
+            type: "nvarchar(max)",
+            nullable: false,
+            defaultValue: "");
+
+        migrationBuilder.AddColumn<int>(
+            name: "MinAge",
+            table: "DefaultChores",
+            type: "int",
+            nullable: false,
+            defaultValue: 0);
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "ChoreDuration",
+            table: "DefaultChores");
+
+        migrationBuilder.DropColumn(
+            name: "Frequency",
+            table: "DefaultChores");
+
+        migrationBuilder.DropColumn(
+            name: "MinAge",
+            table: "DefaultChores");
+
+        migrationBuilder.AddColumn<DateTime>(
+            name: "DueDate",
+            table: "DefaultChores",
+            type: "datetime2",
+            nullable: false,
+            defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+    }
+}
