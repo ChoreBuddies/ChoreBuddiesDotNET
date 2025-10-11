827ffd8712bc6f63f0ce7ea145df99fef3a55a81
kubadrazan <kubadrazan2@gmail.com>
2025-10-11 12:56:44 +0200

    added auth api service

---
+++ ChoreBuddies.Frontend/Features/Authentication/AuthApiService.cs
@@ -0,0 +1,64 @@
+﻿using ChoreBuddies.Frontend.Utilities;
+using Shared.Authentication;
+
+namespace ChoreBuddies.Frontend.Features.Authentication;
+
+public interface IAuthApiService
+{
+    Task<Result> LoginAsync(LoginRequestDto request);
+    Task<Result> RegisterAsync(RegisterRequestDto request);
+    Task RevokeAsync();
+}
+
+public class AuthApiService : IAuthApiService
+{
+    private readonly HttpClientUtils _httpUtils;
+    private readonly IAuthService _authService;
+
+    public AuthApiService(HttpClientUtils httpUtils, IAuthService authService)
+    {
+        _httpUtils = httpUtils;
+        _authService = authService;
+    }
+
+    public async Task<Result> LoginAsync(LoginRequestDto request)
+    {
+        var response = await _httpUtils.TryRequestAsync(
+            () => _httpUtils.PostAsync<LoginRequestDto, AuthResponseDto>(AuthConstants.ApiEndpointLogin, request)
+        );
+
+        if (response?.AccessToken is not null && response?.RefreshToken is not null)
+        {
+            await _authService.LoginAsync(response.AccessToken, response.RefreshToken);
+            return Result.Success();
+        }
+
+        return Result.Fail("Login failed. Please check your credentials.");
+    }
+
+    public async Task<Result> RegisterAsync(RegisterRequestDto request)
+    {
+        var response = await _httpUtils.TryRequestAsync(
+             () => _httpUtils.PostAsync<RegisterRequestDto, AuthResponseDto>(AuthConstants.ApiEndpointSignup, request)
+        );
+
+        if (response?.AccessToken is not null && response?.RefreshToken is not null)
+        {
+            await _authService.LoginAsync(response.AccessToken, response.RefreshToken);
+            return Result.Success();
+        }
+
+        return Result.Fail("Registration failed. Please try again.");
+    }
+
+    public async Task RevokeAsync()
+    {
+        await _httpUtils.TryRequestAsync<object>(async () =>
+        {
+            await _httpUtils.PostAsync(AuthConstants.ApiEndpointRevoke, new { }, authorized: true);
+            return null;
+        });
+
+        await _authService.LogoutAsync();
+    }
+}
--- ChoreBuddies.Frontend/Features/Authentication/AuthConstants.cs
+++ ChoreBuddies.Frontend/Features/Authentication/AuthConstants.cs
@@ -10,4 +10,7 @@ public static class AuthConstants
     public const string UnauthorizedClient = "UnauthorizedClient";
 
     public const string ApiEndpointRefresh = "api/v1/auth/refresh";
+    public const string ApiEndpointRevoke = "api/v1/auth/revoke";
+    public const string ApiEndpointLogin = "api/v1/auth/login";
+    public const string ApiEndpointSignup = "api/v1/auth/signup";
 }
