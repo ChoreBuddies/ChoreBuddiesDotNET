6be89a5afd58ffd9158ae4468947c198cdc37af4
Avesay <juliak@jkkomputer.com.pl>
2025-12-07 21:13:44 +0100

    add all default rewards logic

---
+++ ChoreBuddies.Backend/Features/DefaultRewards/DefaultRewardsController.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Rewards;
+
+namespace ChoreBuddies.Backend.Features.DefaultRewards;
+[Route("api/v1/defaultRewards")]
+[ApiController]
+public class DefaultRewardsController(IDefaultRewardsService defaultRewardsService) : ControllerBase
+{
+    private readonly IDefaultRewardsService _defaultRewardsService = defaultRewardsService;
+    [HttpGet("all")]
+    public async Task<ActionResult<ICollection<DefaultRewardDto>>> GetAllDefaultRewards()
+    {
+        var result = await _defaultRewardsService.GetAllDefaultRewardsAsync();
+        return Ok(result);
+    }
+}
