65234120aa8f7fb3b2a9ab9f6a5666930de8a81a
kubadrazan <kubadrazan2@gmail.com>
2025-12-21 16:53:10 +0100

    change one exception

--- ChoreBuddies.Backend/Infrastructure/Authentication/AuthService.cs
+++ ChoreBuddies.Backend/Infrastructure/Authentication/AuthService.cs
@@ -93,7 +93,7 @@ public class AuthService(UserManager<AppUser> userManager, ITokenService tokenSe
 
         if (user == null)
         {
-            throw new InvalidOperationException("Invalid user identifier.");
+            throw new ArgumentException("Invalid user identifier.");
         }
 
         return await _tokenService.CreateAccessTokenAsync(user);
