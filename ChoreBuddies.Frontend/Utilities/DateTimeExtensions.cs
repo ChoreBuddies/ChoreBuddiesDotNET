634f31d6f282cfe86268857934239d26c24dc23f
Avesay <juliak@jkkomputer.com.pl>
2026-01-03 20:57:00 +0100

    change duedate to at end of day

---
+++ ChoreBuddies.Frontend/Utilities/DateTimeExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace ChoreBuddies.Frontend.Utilities;
+
+public static class DateTimeExtensions
+{
+    public static DateTime AtEndOfDay(this DateTime dt)
+    {
+        return new DateTime(
+            dt.Year,
+            dt.Month,
+            dt.Day,
+            23, 59, 59,
+            dt.Kind
+        );
+    }
+}
