aacd2c0a29344d251bc1dacf73fc3864a8e07054
Avesay <juliak@jkkomputer.com.pl>
2026-01-11 12:17:43 +0100

    Add ScheduledChoresService on frontend

---
+++ ChoreBuddies.Frontend/Features/ScheduledChores/ScheduledChoresService.cs
@@ -0,0 +1,24 @@
+﻿using ChoreBuddies.Frontend.Utilities;
+using Shared.ScheduledChores;
+
+namespace ChoreBuddies.Frontend.Features.ScheduledChores;
+
+public interface IScheduledChoresService
+{
+    public Task<IEnumerable<ScheduledChoreDto>?> GetMyHouseholdChoresAsync();
+}
+public class ScheduledChoresService(HttpClientUtils httpUtils) : IScheduledChoresService
+{
+    public async Task<IEnumerable<ScheduledChoreDto>?> GetMyHouseholdChoresAsync()
+    {
+        var result = await httpUtils.TryRequestAsync(async () =>
+        {
+            return await httpUtils.GetAsync<IEnumerable<ScheduledChoreDto>?>(
+                ScheduledChoresConstants.ApiEndpointGetHouseholdScheduledChores,
+                authorized: true
+            );
+        });
+
+        return result ?? [];
+    }
+}
