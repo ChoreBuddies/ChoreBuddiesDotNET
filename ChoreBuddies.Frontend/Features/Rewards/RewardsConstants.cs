eeaa7934a0ba33a6c8d40889ca2cdfe69dd9177d
Avesay <juliak@jkkomputer.com.pl>
2026-01-11 18:50:59 +0100

    added necessary methods to service

--- ChoreBuddies.Frontend/Features/Rewards/RewardsConstants.cs
+++ ChoreBuddies.Frontend/Features/Rewards/RewardsConstants.cs
@@ -3,4 +3,7 @@
 public static class RewardsConstants
 {
     public static string ApiEndpointGetHouseholdRewards = "/api/v1/rewards/householdRewards?householdId=";
+    public static string ApiEndpointGetRewardById = "/api/v1/rewards/";
+    public static string ApiEndpointAddReward = "/api/v1/rewards/add";
+    public static string ApiEndpointUpdateReward = "/api/v1/rewards/update";
 }
