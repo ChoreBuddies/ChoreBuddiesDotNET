1d565e24e55b8306d33bd63fe081e48a0420552a
kubadrazan <kubadrazan2@gmail.com>
2026-01-15 18:55:23 +0100

    Fixed frontend

---
+++ ChoreBuddies.Frontend/Features/Chat/ChatFrontendConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace ChoreBuddies.Frontend.Features.Chat;
+
+public class ChatFrontendConstants
+{
+    public const string ApiEndpointChatHistory = "api/v1/chat";
+    public const string ApiChatHistoryQueryBefore = "before";
+}
