95c93101bd8db91a15c1755ee85da1716c0555a6
Kobiuy <132697518+Kobiuy@users.noreply.github.com>
2026-01-19 22:37:15 +0100

    file scoped namespace

--- ChoreBuddies.Backend/Infrastructure/Data/Migrations/20260119204657_RemoveMinAgeFromScheduledChores.cs
+++ ChoreBuddies.Backend/Infrastructure/Data/Migrations/20260119204657_RemoveMinAgeFromScheduledChores.cs
@@ -2,28 +2,27 @@
 
 #nullable disable
 
-namespace ChoreBuddies.Backend.Infrastructure.Data.Migrations
+namespace ChoreBuddies.Backend.Infrastructure.Data.Migrations;
+
+/// <inheritdoc />
+public partial class RemoveMinAgeFromScheduledChores : Migration
 {
     /// <inheritdoc />
-    public partial class RemoveMinAgeFromScheduledChores : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "MinAge",
-                table: "ScheduledChores");
-        }
+        migrationBuilder.DropColumn(
+            name: "MinAge",
+            table: "ScheduledChores");
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<int>(
-                name: "MinAge",
-                table: "ScheduledChores",
-                type: "int",
-                nullable: false,
-                defaultValue: 0);
-        }
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AddColumn<int>(
+            name: "MinAge",
+            table: "ScheduledChores",
+            type: "int",
+            nullable: false,
+            defaultValue: 0);
     }
 }
